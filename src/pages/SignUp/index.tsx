2ab44d7a292b0c568234c3516fdb654790e2799e
Gustavo Silva <gstvds@icloud.com>
2020-06-30 22:51:08 -0300

    feat: Adds tooltip

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -21,7 +21,7 @@ const SignUp: React.FC = () => {
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome obrigatório'),
         email: Yup.string()
-          .required('Email obrigatório')
+          .required('E-mail obrigatório')
           .email('Digite um e-mail válido'),
         password: Yup.string().min(6, 'No mínimo 6 dígitos'),
       });
