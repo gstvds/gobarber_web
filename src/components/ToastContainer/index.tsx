f0d0a389ad066b106a11da8591820d2ac4a4c5f6
Gustavo Silva <gstvds@icloud.com>
2020-07-01 22:33:41 -0300

    feat: Separte Toast from ToastContainer and adds auto-remove after 3s

--- src/components/ToastContainer/index.tsx
+++ src/components/ToastContainer/index.tsx
@@ -1,34 +1,19 @@
 import React from 'react';
-import { FiAlertCircle, FiXCircle } from 'react-icons/fi';
 
-import { ToastMessages, useToast } from '../../hooks/toast';
-import { Container, Toast } from './styles';
+import Toast from './Toast';
+
+import { ToastMessages } from '../../hooks/toast';
+import { Container } from './styles';
 
 interface ToastContainerProps {
   messages: ToastMessages[];
 }
 
 const ToastContainer: React.FC<ToastContainerProps> = ({ messages }) => {
-  const { removeToast } = useToast();
-
   return (
     <Container>
       {messages.map(message => (
-        <Toast
-          key={message.id}
-          hasDescription={!!message.description}
-          type={message.type}
-        >
-          <FiAlertCircle size={20} />
-          <div>
-            <strong>{message.title}</strong>
-            {message.description && <p>{message.description}</p>}
-          </div>
-
-          <button type="button" onClick={() => removeToast(message.id)}>
-            <FiXCircle size={18} />
-          </button>
-        </Toast>
+        <Toast key={message.id} toast={message} />
       ))}
     </Container>
   );
